72564d5ef04cf468b2be4631c586557a9445d915
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-20 17:08:12 +0000

    FFS WORK YOU USELESS CUNT

--- src/server.js
+++ src/server.js
@@ -7,6 +7,18 @@ import * as mongodb from './models/mongodb.js';
 
 let app = express();
 
+let allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 // Datbase Connect
 mongoose.Promise = global.Promise;
 mongodb.mongoConnect(mongoose);
