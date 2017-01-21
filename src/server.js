41550f25351cc7f81f28bc39e61f875fb348bc23
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-21 17:03:19 +0000

    CORS

--- src/server.js
+++ src/server.js
@@ -8,20 +8,15 @@ import * as mongodb from './models/mongodb.js';
 let app = express();
 
 let allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    }
-    else {
-      next();
-    }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
 };
 app.use(allowCrossDomain);
 
 // Datbase Connect
 mongoose.Promise = global.Promise;
-mongodb.mongoConnect(mongoose);
+//mongodb.mongoConnect(mongoose);
 
 // Controller Imports
 require('./controllers/exampleController.js')(app);
